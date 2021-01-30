158c2930c4ab5e72b6cccf5e48643950b43a74f6
JUNIORCO <sami.kahil@mail.mcgill.ca>
2021-01-30 16:46:21 -0500

    Initialized project with Spring Boot backend

---
+++ src/main/java/com/ecse428/project/Ecse428ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ecse428.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecse428ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecse428ProjectApplication.class, args);
+	}
+
+}
