3d1a019ce4e160802960eea094c21c971acb1237
VictorrCano <46689150+VictorrCano@users.noreply.github.com>
2021-02-09 11:05:16 -0600

    updated UserController

--- src/main/java/com/ecse428/project/controller/UserController.java
+++ src/main/java/com/ecse428/project/controller/UserController.java
@@ -1,104 +1,37 @@
 package com.ecse428.project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "api/user")
+@RequestMapping(path = "api/user/{userId}")
 public class UserController {
   // TODO Import UserService, write REST endpoints 
   // for adding modifiers & alcohols 
   // TO INVENTORY using UserService methods
 
+  @Autowired
   private final UserService userService;
 
   public UserController (UserService userService){
     this.userService = userService;
   }
 
-  @GetMapping
-  public Alcohol getVodka(){
-    return userService.getVodka();
+
+  @GetMapping("alcohol")
+  public Alcohol getAlcoholInInventory(@PathVariable int userId){
+    return userService.getAlcoholInInventory(userId);
   }
   
-  @PutMapping
-  public void setVodka(Integer amount){
-    this.amount = amount;
+  @PutMapping("alcohol/{alcoholName}")
+  public void addAlcoholToInventory(@PathVariable int userId, @PathVariable String alcoholName){
+    return userService.putAlcoholInInventory(userId, alcoholName);
   }
 
-  @GetMapping
-  public Alcohol getGin(){
-    return userService.getGin();
-  }
-  
-  @PutMapping
-  public void setGin(Integer amount){
-    this.amount = amount;
-  }
 
-  @GetMapping
-  public Alcohol getWhiskey(){
-    return userService.getWhiskey();
-  }
-  
-  @PutMapping
-  public void setWhiskey(Integer amount){
-    this.amount = amount;
-  }
-
-  @GetMapping
-  public Alcohol getTequila(){
-    return userService.getTequila();
-  }
-  
-  @PutMapping
-  public void setTequila(Integer amount){
-    this.amount = amount;
-  }
-
-  @GetMapping
-  public Alcohol getRum(){
-    return userService.getRum();
-  }
-  
-  @PutMapping
-  public void setRum(Integer amount){
-    this.amount = amount;
-  }
-
-  @GetMapping
-  public Alcohol getScoth(){
-    return userService.getSotch();
-  }
-  
-  @PutMapping
-  public void setScoth(Integer amount){
-    this.amount = amount;
-  }
-
-  @GetMapping
-  public Alcohol getBrandy(){
-    return userService.getBrandy();
-  }
-  
-  @PutMapping
-  public void setBrandy(Integer amount){
-    this.amount = amount;
-  }
-
-  @GetMapping
-  public Alcohol getBourbon(){
-    return userService.getBourbon();
-  }
-  
-  @PutMapping
-  public void setBourbon(Integer amount){
-    this.amount = amount;
-  }
-
-  
   //vodka
   //gin
   //whiskey
