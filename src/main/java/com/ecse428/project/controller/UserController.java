b8cb9c0b51edd391057b8cf718adfe83cdb10f64
misha <misha@LAPTOP-5FIDB6FU>
2021-02-10 21:14:36 +0500

    Added methods for modifier

--- src/main/java/com/ecse428/project/controller/UserController.java
+++ src/main/java/com/ecse428/project/controller/UserController.java
@@ -2,11 +2,27 @@ package com.ecse428.project.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
-@RequestMapping(path = "api/user")
+@RequestMapping(path = "api/user/{userId}")
 public class UserController {
-  // TODO Import UserService, write REST endpoints 
-  // for adding modifiers & alcohols 
-  // TO INVENTORY using UserService methods
+
+	  @Autowired
+	  private final UserService userService;
+
+	  public UserController (UserService userService){
+	    this.userService = userService;
+	  }
+	  
+	  @GetMapping("modifier")
+	  public Modifier getModifierInInventory(@PathVariable int userId){
+	    return userService.getModifierInInventory(userId);
+	  }
+	  
+	  @PutMapping("modifier/{modifierName}")
+	  public void addModifierToInventory(@PathVariable int userId, @PathVariable String modifierName){
+	    return userService.putModifierInInventory(userId, modifierName);
+	  }
 }
