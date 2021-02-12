0908e7d8aff1dfd1e3ddcab7eaadba04e09af694
Yiming Wu <yiming.wu@mail.mcgill.ca>
2021-02-11 21:17:39 -0500

    - added more cocktails + ingredients

--- src/main/java/com/ecse428/project/config/CocktailConfig.java
+++ src/main/java/com/ecse428/project/config/CocktailConfig.java
@@ -29,6 +29,8 @@ public class CocktailConfig {
             Alcohol brandy = new Alcohol("Brandy");
             Alcohol bourbon = new Alcohol("Bourbon");
             Alcohol cointreau = new Alcohol("Cointreau");
+            Alcohol champagne = new Alcohol("Champagne");
+            Alcohol tripleSec = new Alcohol("Triple Sec");
 
             Modifier sugarSyrup = new Modifier("Sugar Syrup", ModifierType.SYRUP);
             Modifier limeJuice = new Modifier("Lime Juice", ModifierType.JUICE);
@@ -37,14 +39,21 @@ public class CocktailConfig {
             Modifier tonic = new Modifier("Tonic", ModifierType.JUICE);
             Modifier soda = new Modifier("Soda", ModifierType.JUICE);
             Modifier tomatoJuice = new Modifier("Tomato Juice", ModifierType.JUICE);
+            Modifier orangeJuice = new Modifier("Orange Juice", ModifierType.JUICE);
+            Modifier grenadine = new Modifier("Grenadine", ModifierType.SYRUP);
 
             Cocktail ginAndTonic = new Cocktail("Gin and Tonic", List.of(tonic), List.of(gin));
             Cocktail vodkaSoda = new Cocktail("Vodka Soda", List.of(soda), List.of(vodka));
             Cocktail margarita = new Cocktail("Margarita", List.of(limeJuice), List.of(cointreau, tequila));
+            Cocktail bloodyMary = new Cocktail("Bloody Mary", List.of(tomatoJuice, lemonJuice), List.of(vodka));
+            Cocktail mimosa = new Cocktail("Mimosa", List.of(orangeJuice), List.of(champagne));
+            Cocktail daiquiri = new Cocktail("Daiquiri", List.of(limeJuice, sugarSyrup), List.of(rum));
+            Cocktail tequilaSunrise = new Cocktail("Tequila Sunrise", List.of(orangeJuice, grenadine), List.of(tequila));
+            Cocktail orangeMargarita = new Cocktail("Orange Margarita", List.of(orangeJuice, limeJuice), List.of(tequila, tripleSec));
 
-            alcoholRepository.saveAll(List.of(vodka, gin, whiskey, tequila, rum, scotch, brandy, bourbon, cointreau));
-            modifierRepository.saveAll(List.of(sugarSyrup, limeJuice, lemonJuice, vermouth, tonic, soda, tomatoJuice));
-            cocktailRepository.saveAll(List.of(ginAndTonic, vodkaSoda, margarita));
+            alcoholRepository.saveAll(List.of(vodka, gin, whiskey, tequila, rum, scotch, brandy, bourbon, cointreau, champagne, tripleSec));
+            modifierRepository.saveAll(List.of(sugarSyrup, limeJuice, lemonJuice, vermouth, tonic, soda, tomatoJuice, orangeJuice, grenadine));
+            cocktailRepository.saveAll(List.of(ginAndTonic, vodkaSoda, margarita, bloodyMary, mimosa, daiquiri, tequilaSunrise, orangeMargarita));
         };
     }
 }
