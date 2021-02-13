0a9134928b9f176e4823cc85e90fbadea1a19da6
JUNIORCO <sami.kahil@mail.mcgill.ca>
2021-02-13 16:18:20 -0500

    Add alcohol type to inventory controller tested

---
+++ src/test/java/com/ecse428/project/unit/controller/AlcoholControllerIntegrationTest.java
@@ -0,0 +1,52 @@
+package com.ecse428.project.unit.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.ecse428.project.controller.AlcoholController;
+import com.ecse428.project.model.Alcohol;
+import com.ecse428.project.service.IAlcoholService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(AlcoholController.class)
+public class AlcoholControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private IAlcoholService service;
+
+    @Test
+    public void givenAlcohol_whenGetAlcohols_thenReturnJsonArray() throws Exception {
+
+        Alcohol vodka = new Alcohol("Vodka");
+
+        List<Alcohol> allAlcohols = Arrays.asList(vodka);
+
+        given(service.getAlcohol()).willReturn(allAlcohols);
+
+        mvc.perform(MockMvcRequestBuilders
+                .get("/api/alcohol")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].name", is(vodka.getName())));
+    }
+}
