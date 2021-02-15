cc765445b0ee88ae7cbaf76f0954340c96247d7a
Haowei <haowei.qiu@mail.mcgill.ca>
2021-02-14 20:33:36 -0500

    fix

--- src/test/java/com/ecse428/project/acceptance/steps/scenarioSteps/CreateAccount/ValidCreateAccountSteps.java
+++ src/test/java/com/ecse428/project/acceptance/steps/scenarioSteps/CreateAccount/ValidCreateAccountSteps.java
@@ -1,4 +1,4 @@
-package com.ecse428.project.acceptance.steps.scenarioSteps.addModifierToInventory;
+package com.ecse428.project.acceptance.steps.scenarioSteps.createAccount;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
