1a97f732ae325f44416920a9c799627cc3221cc6
Louis Mollick <louismollick@gmail.com>
2021-02-18 16:00:24 -0500

    Set up client, basic structure & all needed dependencies

---
+++ client/src/views/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const Home = () => {
+  return (
+    <div>
+      <Typography component="h1" variant="h5" align="center">
+        Welcome to mixer, the best cocktail generator on the internet
+      </Typography>
+    </div>
+  )
+};
+
+export default Home;
