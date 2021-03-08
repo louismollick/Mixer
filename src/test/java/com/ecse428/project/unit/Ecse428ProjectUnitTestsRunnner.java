ac4cb4b94bbe4f5f7478841b2ab2323df558ab6f
misha <misha@LAPTOP-5FIDB6FU.mshome.net>
2021-03-08 06:13:13 +0500

    working on unit tests

--- src/test/java/com/ecse428/project/unit/Ecse428ProjectUnitTestsRunnner.java
+++ src/test/java/com/ecse428/project/unit/Ecse428ProjectUnitTestsRunnner.java
@@ -1,12 +1,12 @@
 package com.ecse428.project.unit;
 
-import com.ecse428.project.unit.service.IModifierServiceUnitTest;
+import com.ecse428.project.unit.service.IUserServiceUnitTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ IModifierServiceUnitTest.class })
+@SuiteClasses({ IUserServiceUnitTest.class })
 public class Ecse428ProjectUnitTestsRunnner {
 }
