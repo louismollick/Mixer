9d4e614379078d2a6b888d705e2d40a3c21aa695
Yiming Wu <yiming.wu@mail.mcgill.ca>
2021-03-28 20:55:50 -0400

    Added Misha's Favourite/Unfavourite cocktail gherkin tests

---
+++ src/test/java/com/ecse428/project/acceptance/steps/scenarioSteps/favouriteACocktail/favouriteACocktailSteps.java
@@ -0,0 +1,48 @@
+package com.ecse428.project.acceptance.steps.scenarioSteps.favouriteACocktail;
+
+import io.cucumber.java.en.*;
+
+import static org.junit.Assert.assertTrue;
+
+public class favouriteACocktailSteps {
+
+    @Given("^I am signed up for Mixer$")
+    public void i_am_signed_up_for_mixer() {
+        assertTrue(true);
+    }
+
+    @And("^I am logged into my account$")
+    public void i_am_logged_into_my_account() {
+        assertTrue(true);
+    }
+
+    @When("^I search a coctail$")
+    public void i_search_a_coctail() {
+        assertTrue(true);
+    }
+
+    @And("^I favorite the cocktail$")
+    public void i_favorite_the_cocktail() {
+        assertTrue(true);
+    }
+
+    @Then("^the coctail is added to my favorites list$")
+    public void the_coctail_is_added_to_my_favorites_list() {
+        assertTrue(true);
+    }
+
+    @And("^the cocktail is already in my favorites list$")
+    public void the_cocktail_is_already_in_my_favorites_list() {
+        assertTrue(true);
+    }
+
+    @Then("^the coctail is kept in my favorites list$")
+    public void the_coctail_is_kept_in_my_favorites_list() {
+        assertTrue(true);
+    }
+
+    @Then("^the coctail is not added to my favorites list$")
+    public void the_coctail_is_not_added_to_my_favorites_list() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/com/ecse428/project/acceptance/steps/scenarioSteps/unfavouriteACocktail/unfavouriteACocktailSteps.java
@@ -0,0 +1,51 @@
+package com.ecse428.project.acceptance.steps.scenarioSteps.unfavouriteACocktail;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+import static org.junit.Assert.assertTrue;
+
+public class unfavouriteACocktailSteps {
+    /*
+    @Given("^I am signed up for Mixer$")
+    public void iAmSignedUpForMixer() {
+        assertTrue(true);
+    }*/
+
+    /*
+    @And("^I am logged into my account$")
+    public void iAmLoggedIntoMyAccount() {
+        assertTrue(true);
+    }*/
+
+    /*
+    @When("^I search a coctail$")
+    public void iSearchACoctail() {
+        assertTrue(true);
+    }*/
+
+    @And("^I unfavorite the cocktail$")
+    public void iUnfavoriteTheCocktail() {
+        assertTrue(true);
+    }
+
+    @Then("^the coctail is removed from my favorites list$")
+    public void theCoctailIsRemovedFromMyFavoritesList() {
+        assertTrue(true);
+    }
+
+    @And("^the cocktail is already not in my favorites list$")
+    public void theCocktailIsAlreadyNotInMyFavoritesList() {
+        assertTrue(true);
+    }
+
+    @Then("^the coctail is still not in my favorites list$")
+    public void theCoctailIsStillNotInMyFavoritesList() {
+        assertTrue(true);
+    }
+
+    @Then("^the coctail is not removed from my favorites list$")
+    public void theCoctailIsNotRemovedFromMyFavoritesList() {
+        assertTrue(true);
+    }
+}
